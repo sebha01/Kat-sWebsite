47c055a278e26030b80a5062b6f703133e085952
sebha01 <30077548@students.southwales.ac.uk>
2025-09-12 09:26:48 +0100

    Restarting but with vite and react

--- script.js
+++
@@ -1,6 +0,0 @@
-const navToggle = document.getElementById("nav-toggle");
-const navbar = document.getElementById("navbar");
-
-navToggle.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-});
